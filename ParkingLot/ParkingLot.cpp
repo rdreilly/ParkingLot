3e19ee6b5f0f9cef2a8af32e0215c5265fcd17ed
BobReilly <bobreilly2015@gmail.com>
2016-10-14 14:35:35 -0400

    Updated with implementation

--- ParkingLot/ParkingLot.cpp
+++ ParkingLot/ParkingLot.cpp
@@ -18,3 +18,48 @@ int ParkingLot::getN() const
 	return n;
 }
 
+vehicle_type_t Vehicle::getVehicleType()
+{
+	return vehicle_type;
+}
+Vehicle::Vehicle(vehicle_type_t Vehicle)
+{
+	vehicle_type = Vehicle;
+}
+ParkingSlot::ParkingSlot(int id, vehicle_type_t supported_vehicle )
+{	
+	id = id;
+	supported_vehicle = supported_vehicle;
+}
+bool ParkingSlot::isReserved()
+{
+	return reserved;
+}
+int ParkingSlot::reserve(Vehicle vehicle)
+{
+	if (vehicle.getVehicleType() != supported_vehicle)
+		throw SLOT_ERR;
+	reserved = true;
+	vehicle = vehicle;
+	return id;
+}
+Vehicle ParkingSlot::unreserve()
+{
+	reserved = false;
+	return vehicle;
+}
+ParkingLot::ParkingLot(int reg, int hand, int elec, int compact)
+{
+	int slotId = 0;
+	for (int i = 0; i < reg; i++)
+		addParkingSlot(++slotId, REG);
+	for (int i = 0; i < hand; i++)
+		addParkingSlot(++slotId, HANDICAPPED);
+	for (int i = 0; i < elec; i++)
+		addParkingSlot(++slotId, ELEC);
+	for (int i = 0; i < compact; i++)
+		addParkingSlot(++slotId, COMPACT);
+	empty_slots = parkingSlots.size();
+}
+
+
--- ParkingLot/ParkingLot.h
+++ ParkingLot/ParkingLot.h
@@ -1,9 +1,42 @@
 #pragma once
+#include <vector>
+#include <array>
+#include <map>
+
+using namespace std;
 
 class ParkingLot
 {
 	int n = 10;
+	map <int, ParkingSlot> parkingSlots;
 public:
+	int empty_slots;
 	static int getTwo();
 	int getN() const;
+	ParkingLot(int regSlots, int handSlots, int elecSlots, int compSlots);
+	void addParkingSlot(int id, vehicle_type_t vehicle_type);
+};
+
+enum vehicle_type_t {REG, HANDICAPPED, ELEC, COMPACT};
+#define SLOT_ERR -77;
+class Vehicle
+{
+	vehicle_type_t vehicle_type;
+public:
+	vehicle_type_t getVehicleType();
+	Vehicle(vehicle_type_t Vehicle);
+};
+
+class ParkingSlot
+{
+	int id;
+	bool reserved = false;
+	vehicle_type_t vehicle;
+	vehicle_type_t supported_vehicle;
+public:
+	vehicle_type_t get_supported_vehicle_type();
+	ParkingSlot(int id, vehicle_type_t supported_vehicle);
+	bool isReserved();
+	int reserve(Vehicle vehicle);
+	Vehicle unreserve();
 };
